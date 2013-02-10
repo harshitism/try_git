ad4e4a8b780be66323ae88cd537da492daf604f9
HARSHIT GUPTA <harshit.shift@gmail.com>
2013-02-10 15:46:06 +0530

    Included index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>BINGO</title>
+</head>
+<body>
+<h1>BINGO</h1>
+<a href="register.php">REGISTER</a><br/>
+<a href="login.php">LOGIN</a>
+</body>
+</html>
