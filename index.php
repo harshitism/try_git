93dc7fc993af03aca1293fca7e926f64eb77bc91
HARSHIT GUPTA <harshit.shift@gmail.com>
2013-03-20 19:35:34 +0530

    Revert "Complete Project"
    This reverts commit 88eea581075b74e665e7c444d547a8ede7455eb4.

--- index.php
+++ index.php
@@ -1,125 +1,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>AF</title>
-<style type="text/css">
-#apDiv1 {
-	position:absolute;
-	width:1000px;
-	height:100px;
-	z-index:1;
-	left: 200px;
-	top: 0px;
-	font-size: 50px;
-	font-family: Georgia, "Times New Roman", Times, serif;
-	text-align: center;
-	background-color: #999999;
-	color: #F00;
-}
-#apDiv2 {
-	position:absolute;
-	width:200px;
-	height:800px;
-	z-index:1;
-	left: 0px;
-	top: 0px;
-	background-color: #666666;
-}
-#apDiv3 {
-	position:absolute;
-	width:800px;
-	height:115px;
-	z-index:2;
-	left: 0px;
-	top: 200px;
-}
-#main {
-	position: absolute;
-	top: 100px;
-	left: 200px;
-	width: 650px;
-	height: 650px;
-}
-a {
-	text-decoration: none;
-	color: #FFF;
-}
-a:hover {
-	color: #F00;
-}
-#apDiv4 {
-	position:absolute;
-	width:100px;
-	height:30px;
-	z-index:1;
-	left: 50px;
-	top: 130px;
-	text-align: center;
-	background-color: #36F;
-}
-#apDiv5 {
-	position:absolute;
-	width:100px;
-	height:30px;
-	z-index:2;
-	left: 50px;
-	top: 210px;
-	text-align: center;
-	background-color: #36f;
-}
-#apDiv6 {
-	position:absolute;
-	width:100px;
-	height:30px;
-	z-index:3;
-	left: 50px;
-	top: 290px;
-	text-align: center;
-	background-color: #36F;
-}
-#apDiv7 {
-	position:absolute;
-	width:100px;
-	height:30px;
-	z-index:2;
-	left: 50px;
-	top: 720px;
-	text-align: center;
-	background-color: #36F;
-}
-#apDiv8 {
-	position:absolute;
-	width:1000px;
-	height:50px;
-	z-index:2;
-	left: 200px;
-	top: 750px;
-	opacity:0.8;
-	background-color: #999999;
-}
-#apDiv9 {
-	position: absolute;
-	left: 50px;
-	top: 50px;
-	width: 100px;
-	height: 30px;
-	background-color: #36F;
-	text-align: center;
-}
-</style>
+<title>BINGO</title>
 </head>
-
 <body>
-<div id="apDiv1">ANSHU FASHION</div>
-  <div id="apDiv2">
-    <a href="login.php" target="main"><div id="apDiv9">LOGIN</div></a>
-    <a href="entry.php" target="main"><div id="apDiv4">ENTRY</div></a>
-    <a href="sale.php" target="main"><div id="apDiv5">SALE</div></a>
-    <a href="stock.php" target="main"><div id="apDiv6">STOCK</div></a>
-    <a href="logout.php"><div id="apDiv7"><b>LOGOUT</b></div></a>
-</div>
-  <iframe id="main" name="main" frameborder="0" src="src.php"></iframe>
-  <div id="apDiv8">&copy; copyright 2012 ANSHU FASHION  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Designed by-<a href="harshit.php">HARSHIT GUPTA</a></div>
+<h1>BINGO</h1>
+<a href="register.php">REGISTER</a><br/>
+<a href="login.php">LOGIN</a>
 </body>
 </html>
